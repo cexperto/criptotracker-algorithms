a5d148f04f371d7cbcdedc3bc7e2e0298b6dcbe6
cexperto <sipuedeshazloya@gmail.com>
2021-07-01 15:59:17 -0500

    try to fix settings

--- cryptoTracker/settings.py
+++ cryptoTracker/settings.py
@@ -128,11 +128,13 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = {
+STATICFILES_DIRS = (
+
     os.path.join(BASE_DIR, 'static'),
-}
+
+)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
